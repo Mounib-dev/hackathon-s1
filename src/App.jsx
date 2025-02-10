486d8eafb761eeb6a9745722fd18ce0abfcbb9a7
l3miage-larbiy <larbi1992yasmine@gmail.com>
2025-02-10 15:52:36 +0100

    Add config formbesoin

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/layout/Navbar";
+import FormBesoin from "./components/Accueil/FormBesoin";
+
 function App() {
   return (
     <>
-      <div>Test</div>
+
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<FormBesoin />}></Route>
+        </Routes>
+      </Router>
+
     </>
   );
 }
