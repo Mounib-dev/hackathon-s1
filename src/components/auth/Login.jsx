99ad5acf2fe576430c63c8e4e73437f3bf42d46c
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-02-12 15:34:56 +0100

    Socket config centralized

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { useAuth } from "../../context/AuthContext";
+import { Eye, EyeOff } from "lucide-react";
 // import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const { login } = useAuth();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   //   const navigate = useNavigate();
 
   const handleSubmit = async (event) => {
@@ -43,17 +45,27 @@ const Login = () => {
                 required
               />
             </div>
-            <div>
+
+            {/* Password Input with Eye Icon */}
+            <div className="relative mt-4">
               <label className="mb-3 block text-lg font-semibold text-indigo-700">
                 Password:
               </label>
               <input
-                className="focus:shadow-outline w-full appearance-none rounded-md border px-4 py-3 leading-tight text-indigo-700 shadow-md focus:border-indigo-500 focus:outline-none"
-                type="password"
+                className="w-full rounded-md border px-4 py-3 pr-12 text-indigo-700 shadow-md focus:border-indigo-500 focus:outline-none"
+                type={showPassword ? "text" : "password"}
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 required
               />
+              {/* Eye Button */}
+              <button
+                type="button"
+                className="absolute top-1/2 right-3 translate-y-1/2 transform text-indigo-500 hover:text-indigo-700"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+              </button>
             </div>
             <div className="mt-8 flex items-center justify-between">
               <button
