4cbeaa40115848effb9fc404bbfe00a176c6cbe6
l3miage-larbiy <larbi1992yasmine@gmail.com>
2025-03-10 10:35:09 +0100

    add map

--- src/components/FormBesoin.jsx
+++ src/components/FormBesoin.jsx
@@ -6,6 +6,8 @@ api.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('t
 function FormBesoin() {
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
+    const [category, setCategory] = useState('accident');
+    const [customCategory, setCustomCategory] = useState('');
     const [priorityLevel, setPriorityLevel] = useState('faible');
     const [location, setLocation] = useState('');
 
@@ -16,17 +18,20 @@ function FormBesoin() {
             const response = await api.post(import.meta.env.VITE_API_BASE_URL + Create_alertsEndpoint, {
                 title,
                 description,
+                category: category === 'autre' ? customCategory : category,
                 priorityLevel,
                 location,
             }, { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } });
+            console.log(response)
 
             setTitle('');
             setDescription('');
+            setCategory('accident');
+            setCustomCategory('');
             setPriorityLevel('faible');
             setLocation('');
 
-            //fetchAlerts(); // Rafraîchir la liste après soumission
-            console.log("le besoin est bien enregistré", response)
+            console.log("Le besoin est bien enregistré", response);
         } catch (error) {
             console.error('Erreur lors de la soumission:', error);
         }
@@ -38,16 +43,56 @@ function FormBesoin() {
                 <h2 className="text-2xl font-semibold mb-6 text-gray-900">Saisir votre besoin</h2>
                 <fieldset className="space-y-6">
                     <div>
+                        <label className="block text-sm font-medium text-gray-700 mb-2">Catégorie</label>
+                        <select
+                            value={category}
+                            onChange={(e) => setCategory(e.target.value)}
+                            className="bg-gray-50 border border-pink-300 rounded-lg w-full px-4 py-2 text-sm"
+                        >
+                            <option value="urgence">Urgence</option>
+                            <option value="Panne">Panne</option>
+                            <option value="Accident">Accident</option>
+                            <option value="Catastrophe Naturelle">Catastrophe Naturelle</option>
+                            <option value="Autre">Autre</option>
+                        </select>
+                        {category === 'Autre' && (
+                            <input
+                                type="text"
+                                value={customCategory}
+                                onChange={(e) => setCustomCategory(e.target.value)}
+                                placeholder="Entrez une catégorie"
+                                className="bg-gray-50 border border-pink-300 rounded-lg w-full px-4 py-2 text-sm mt-2"
+                                required
+                            />
+                        )}
+                    </div>
+                    <div>
                         <label className="block text-sm font-medium text-gray-700 mb-2">Titre du besoin</label>
-                        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} className="bg-gray-50 border border-pink-300 rounded-lg w-full px-4 py-2 text-sm" required />
+                        <input
+                            type="text"
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                            className="bg-gray-50 border border-pink-300 rounded-lg w-full px-4 py-2 text-sm"
+                            required placeholder='Saisir un titre'
+                        />
                     </div>
                     <div>
                         <label className="block text-sm font-medium text-gray-700 mb-2">Description du besoin</label>
-                        <textarea value={description} onChange={(e) => setDescription(e.target.value)} className="bg-gray-50 border border-pink-300 rounded-lg w-full px-4 py-2 text-sm h-32" required />
+                        <textarea
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            className="bg-gray-50 border border-pink-300 rounded-lg w-full px-4 py-2 text-sm h-32"
+                            required placeholder='Saisir une description...'
+                        />
                     </div>
+
                     <div>
                         <label className="block text-sm font-medium text-gray-700 mb-2">Niveau de priorité</label>
-                        <select value={priorityLevel} onChange={(e) => setPriorityLevel(e.target.value)} className="bg-gray-50 border border-pink-300 rounded-lg w-full px-4 py-2 text-sm">
+                        <select
+                            value={priorityLevel}
+                            onChange={(e) => setPriorityLevel(e.target.value)}
+                            className="bg-gray-50 border border-pink-300 rounded-lg w-full px-4 py-2 text-sm"
+                        >
                             <option value="faible">Faible</option>
                             <option value="moyen">Moyen</option>
                             <option value="critique">Critique</option>
@@ -55,15 +100,19 @@ function FormBesoin() {
                     </div>
                     <div>
                         <label className="block text-sm font-medium text-gray-700 mb-2">Adresse de l'utilisateur</label>
-                        <input type="text" value={location} onChange={(e) => setLocation(e.target.value)} className="bg-gray-50 border border-pink-300 rounded-lg w-full px-4 py-2 text-sm" required />
+                        <input
+                            type="text"
+                            value={location}
+                            onChange={(e) => setLocation(e.target.value)}
+                            className="bg-gray-50 border border-pink-300 rounded-lg w-full px-4 py-2 text-sm"
+                            required placeholder='Saisir votre adresse '
+                        />
                     </div>
                     <div>
                         <button type="submit" className="w-full py-2 px-4 bg-pink-500 text-white rounded-lg hover:bg-pink-700">Soumettre</button>
                     </div>
                 </fieldset>
             </form>
-
-
         </div>
     );
 }
