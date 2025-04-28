ed918fbdbc624b5d29709e943a21afb786bec0a8
l3miage-larbiy <larbi1992yasmine@gmail.com>
2025-04-28 08:24:46 +0200

    add note

--- src/components/Volontaires.jsx
+++ src/components/Volontaires.jsx
@@ -4,30 +4,58 @@ import api from "../axiosConfig";
 import { FaStar } from "react-icons/fa";
 
 function Volontaires() {
+  const [userId, setUserId] = useState(() =>
+    Number(localStorage.getItem("userId"))
+  );
   const { user } = useAuth();
   const [users, setUsers] = useState([]);
   const [ratings, setRatings] = useState({});
+  const [ratedUsers, setRatedUsers] = useState(new Set()); // Pour suivre les volontaires déjà notés
 
   useEffect(() => {
-   
     const fetchUsers = async () => {
-        const response = await api.get(import.meta.env.VITE_API_BASE_URL + "/user/list"); 
-        setUsers(response.data);
-          
-              
-  
+      try {
+        const response = await api.get(
+          import.meta.env.VITE_API_BASE_URL + "/user/list"
+        );
+        // Filtrer les utilisateurs pour exclure l'utilisateur connecté
+        const filteredUsers = response.data.filter((u) => u.id !== userId);
+        setUsers(filteredUsers);
+      } catch (error) {
+        console.error("Erreur lors de la récupération des utilisateurs", error);
+      }
     };
-   
     fetchUsers();
   }, [user]);
 
+  const handleRating = async (userId, rating) => {
+    // Vérifier si l'utilisateur a déjà noté ce volontaire
+    if (ratedUsers.has(userId)) {
+      alert("Vous avez déjà noté ce volontaire.");
+      return;
+    }
 
-
-  const handleRating = (userId, rating) => {
+   
     setRatings((prev) => ({
       ...prev,
       [userId]: rating,
     }));
+
+    try {
+      const rate = await api.post(
+        import.meta.env.VITE_API_BASE_URL + "/note/create",
+        {
+          user_id: userId,
+          rating: rating,
+        }
+      );
+      console.log(rate);
+      
+      // Ajouter ce volontaire à la liste des volontaires notés
+      setRatedUsers((prev) => new Set(prev).add(userId));
+    } catch (error) {
+      console.error("Erreur lors de l'envoi de la note", error);
+    }
   };
 
   return (
@@ -49,7 +77,6 @@ function Volontaires() {
                   <h4 className="text-2xl font-semibold text-gray-800">
                     {u.firstName} {u.lastName}
                   </h4>
-                 
                 </div>
               </div>
               <div className="mt-4">
@@ -69,7 +96,8 @@ function Volontaires() {
                 </div>
                 {ratings[u.id] && (
                   <p className="text-sm text-gray-500 mt-2">
-                    Note attribuée : {ratings[u.id]} étoile{ratings[u.id] > 1 ? "s" : ""}
+                    Note attribuée : {ratings[u.id]} étoile
+                    {ratings[u.id] > 1 ? "s" : ""}
                   </p>
                 )}
               </div>
